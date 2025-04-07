6de1d223225028f682d151184bb086c837a1705d
ajayi daves <ajayid89@gmail.com>
2025-04-07 14:40:39 +0200

    refactor: update MobileMenu layout and remove commented navigation links

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -14,12 +14,12 @@ const MobileMenu = ({ isOpen }: MobileMenuProps) => {
           exit={{ y: '-100%', opacity: 0 }} // Exits in the opposite direction
           transition={{ duration: 0.4, ease: 'easeInOut' }}
           onClick={(e) => e.stopPropagation()} // Prevents closing when clicking inside
-          className='fixed top-30 left-0 z-50 flex h-80 w-full flex-col items-center justify-center gap-6 bg-base-100 shadow-lg'
+          className='fixed top-30 left-0 z-50 flex  w-full flex-col items-center justify-center gap-6 bg-base-100 shadow-lg'
         >
           {/* Navigation Links */}
 
-
-          <ul className='flex flex-col gap-4 w-full'>
+          {/* <ul className='flex flex-col gap-4 w-full'>
+            <li className='p-4 border-b border-[#efe9ec] flex items-center justify-center static uppercase font-extralight'></li>
             {['Home', 'Shop', 'About', 'Contact'].map((item) => (
               <li
                 className='p-4 border-b border-[#efe9ec] flex items-center justify-center static uppercase font-extralight'
@@ -30,7 +30,20 @@ const MobileMenu = ({ isOpen }: MobileMenuProps) => {
                 </a>
               </li>
             ))}
-          </ul>
+          </ul> */}
+          <div className='grid w-full items justify-items-stretch  justify-between auto-cols-fr auto-rows-auto gap-x-4 gap-y-0'>
+            <div className='grid auto-cols-auto grid-cols-[auto] grid-rows-[auto] gap-x-13.5 py-0 w-full grid-flow-row bg-white text-black border-t-[#e4e9ec] gap-y-0 self-center col-start-1 col-end-2 row-start-1 row-end-2'>
+              {['Home', 'Shop', 'About', 'Contact'].map((item) => (
+                <a
+                  id={item}
+                  className='text-center max-w-full static flex decorate-none p-0 text-[12px] leading-[1.2em] mx-0 h-full items-center justify-center uppercase tracking-[5px] py-6 text-black border-b-[#e4e9ec] border-b  col-span- row-span-1'
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <div></div>
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
